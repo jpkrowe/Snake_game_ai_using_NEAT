9f017d7065472bc09255a69caafdbada58b699b9
Jamie Rowe <jr774@cam.ac.uk>
2022-06-13 01:19:41 +0100

    Changed to use numpy array for coordinates. Changed information fed to the neural network

--- snake game.py
+++ snake game.py
@@ -1,3 +1,5 @@
+import pickle
+
 import pygame
 import neat
 import os
@@ -7,13 +9,13 @@ pygame.font.init()
 
 
 # define screen size
-GRID_X = 50
-GRID_Y = 50
-SNAKE_WIDTH = 10
+GRID_X = 25
+GRID_Y = 25
+SNAKE_WIDTH = 20
 WIN_WIDTH = GRID_X * SNAKE_WIDTH
 WIN_HEIGHT = GRID_Y * SNAKE_WIDTH
-SNAKE_INIT_X = 10
-SNAKE_INIT_Y = 10
+# SNAKE_INIT_X = 10
+# SNAKE_INIT_Y = 10
 
 INIT_SNAKE_LENGTH = 6
 
@@ -26,30 +28,22 @@ STAT_FONT = pygame.font.SysFont("comicsans", 50)
 class Head:
 
     def __init__(self, xcoord, ycoord):
-        self.xcoord = xcoord
-        self.ycoord = ycoord
-        self.x = self.xcoord * SNAKE_WIDTH
-        self.y = self.ycoord * SNAKE_WIDTH
-        self.travelDirection = [1, 0]
-        self.previous_x = 0
-        self.previous_y = 0
-        self.rect = pygame.Rect(self.x, self.y, SNAKE_WIDTH, SNAKE_WIDTH)
-        self.previous_xcoord = 0
-        self.previous_ycoord = 0
+        self.coords = np.array([xcoord, ycoord])
+        self.pos = self.coords * SNAKE_WIDTH
+        self.travelDirection = np.array(random.choice([[0, 1], [0, -1], [1, 0], [-1, 0]]))
+        self.previous_pos = np.array([0, 0])
+        self.rect = pygame.Rect(self.pos[0], self.pos[1], SNAKE_WIDTH, SNAKE_WIDTH)
+        self.previous_coords = self.coords - self.travelDirection
 
     def move(self):
-        self.previous_xcoord = self.xcoord
-        self.previous_ycoord = self.ycoord
-
-        self.xcoord = self.xcoord + self.travelDirection[0]
-        self.ycoord = self.ycoord + self.travelDirection[1]
-        self.x = self.xcoord * SNAKE_WIDTH
-        self.y = self.ycoord * SNAKE_WIDTH
-        self.rect = pygame.Rect(self.x, self.y, SNAKE_WIDTH, SNAKE_WIDTH)
+        self.previous_coords = self.coords
+        self.coords = self.coords + self.travelDirection
+        self.pos = self.coords * SNAKE_WIDTH
+        self.rect = pygame.Rect(self.pos[0], self.pos[1], SNAKE_WIDTH, SNAKE_WIDTH)
 
     def rotate(self, rotate):
 
-        if rotate == 1: # clockwise turn
+        if rotate == 1:  # clockwise turn
             if self.travelDirection[1] == 0:
                 placeholder = self.travelDirection[0]
                 self.travelDirection[0] = self.travelDirection[1]
@@ -69,7 +63,7 @@ class Head:
                 self.travelDirection[1] = placeholder
 
     def draw(self, win):
-        pygame.draw.rect(win, (255, 0, 0), (self.x, self.y, SNAKE_WIDTH, SNAKE_WIDTH))
+        pygame.draw.rect(win, (255, 0, 0), (self.pos[0], self.pos[1], SNAKE_WIDTH, SNAKE_WIDTH))
 
     def sense(self, rotoption):  # generates the next potential position of the head piece
         testDirection = self.travelDirection
@@ -92,40 +86,34 @@ class Head:
                 testDirection[0] = testDirection[1]
                 testDirection[1] = placeholder
 
-        testcoord = [self.xcoord + testDirection[0], self.ycoord + testDirection[1]]
+        testcoord = self.coords + testDirection
         return testcoord
 
 
 
 class Body:
-    def __init__(self, xcoord, ycoord):
-        self.xcoord = xcoord
-        self.ycoord = ycoord
-        self.x = self.xcoord * SNAKE_WIDTH
-        self.y = self.ycoord * SNAKE_WIDTH
-        self.rect = pygame.Rect(self.x, self.y, SNAKE_WIDTH, SNAKE_WIDTH)
+    def __init__(self, starting_coords):
+        self.coords = starting_coords
+        self.pos = self.coords * SNAKE_WIDTH
+        self.rect = pygame.Rect(self.pos[0], self.pos[1], SNAKE_WIDTH, SNAKE_WIDTH)
 
     def move(self, head):
-        self.xcoord = head.previous_xcoord
-        self.ycoord = head.previous_ycoord
-        self.x = self.xcoord*SNAKE_WIDTH
-        self.y = self.ycoord*SNAKE_WIDTH
-        self.rect = pygame.Rect(self.x, self.y, SNAKE_WIDTH, SNAKE_WIDTH)
+        self.coords = head.previous_coords
+        self.pos = self.coords * SNAKE_WIDTH
+        self.rect = pygame.Rect(self.pos[0], self.pos[1], SNAKE_WIDTH, SNAKE_WIDTH)
 
     def draw(self, win):
-        pygame.draw.rect(win, (255, 0, 0), (self.x, self.y, SNAKE_WIDTH, SNAKE_WIDTH))
+        pygame.draw.rect(win, (255, 0, 0), (self.pos[0], self.pos[1], SNAKE_WIDTH, SNAKE_WIDTH))
 
 
 class Food:
     def __init__(self):
-        self.xcoord = random.randint(0, GRID_X-1)
-        self.ycoord = random.randint(0, GRID_Y-1)
-        self.x = self.xcoord*SNAKE_WIDTH
-        self.y = self.ycoord*SNAKE_WIDTH
-        self.rect = pygame.Rect(self.x, self.y, SNAKE_WIDTH, SNAKE_WIDTH)
+        self.coords = np.array([random.randint(0, GRID_X-1), random.randint(0, GRID_Y-1)])
+        self.pos = self.coords * SNAKE_WIDTH
+        self.rect = pygame.Rect(self.pos[0], self.pos[1], SNAKE_WIDTH, SNAKE_WIDTH)
 
     def draw(self, win):
-        pygame.draw.rect(win, (255, 0, 0), (self.x, self.y, SNAKE_WIDTH, SNAKE_WIDTH))
+        pygame.draw.rect(win, (255, 0, 0), (self.pos[0], self.pos[1], SNAKE_WIDTH, SNAKE_WIDTH))
 
 
 def draw_window(win, snake, food, score):
@@ -152,13 +140,14 @@ def main(genomes, config):
     for _, g in genomes:
         net = neat.nn.FeedForwardNetwork.create(g, config)
         nets.append(net)
-        snake = [Head(SNAKE_INIT_X, SNAKE_INIT_Y)]
+        starting_x, starting_y = random.randint(INIT_SNAKE_LENGTH + 1, GRID_X-INIT_SNAKE_LENGTH), random.randint(0, GRID_Y-INIT_SNAKE_LENGTH)
+        snake = [Head(starting_x, starting_y)]
+        initial_direction = snake[0].travelDirection
+        for i in range(1, INIT_SNAKE_LENGTH + 1):
+            snake.append(Body(snake[0].coords - i * initial_direction))
         snakes.append(snake)
         g.fitness = 0
         ge.append(g)
-    for snake in snakes:
-        for i in range(1, INIT_SNAKE_LENGTH + 1):
-            snake.append(Body(SNAKE_INIT_X - i, SNAKE_INIT_Y))
 
 
     win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
@@ -173,9 +162,11 @@ def main(genomes, config):
         noRotAntiClock = 0
         missFood = 0
 
-
         while alive:
             clock.tick(500)
+            win.fill((0, 0, 0))
+            draw_window(win, snake, food, score)
+            pygame.display.update()
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
@@ -192,16 +183,18 @@ def main(genomes, config):
                 collisionTest = snake[0].sense(rotoption)
                 # checks if the snake will hit itself
                 for body in snake[1:]:
-                    if collisionTest[0] == body.xcoord and collisionTest[1] == body.ycoord:
+                    if np.all(collisionTest == body.coords):
                         inputs[rotoption] = 1
 
                 # checks if the snake will hit a wall
                 if collisionTest[0] < 0 or collisionTest[1] < 0 or collisionTest[0] >= GRID_X or collisionTest[1] >= GRID_Y:
                     inputs[rotoption] = 1
 
-            foodDistance = (snake[0].xcoord - food.xcoord)**2 + (snake[0].ycoord - food.ycoord)**2
+            foodDistance = snake[0].coords - food.coords
+            currentTravel = snake[0].travelDirection
 
-            output = np.array(nets[i].activate((foodDistance, inputs[0], inputs[1], inputs[2])))
+            output = np.array(nets[i].activate((foodDistance[0], foodDistance[1], currentTravel[0], currentTravel[1],
+                                                inputs[0], inputs[1], inputs[2])))
             # represent outputs as softmax probabilities
             probs = np.exp(output)/np.exp(output).sum()
             choice = probs.argmax()
@@ -214,12 +207,9 @@ def main(genomes, config):
             snake[-1].move(snake[0])
             snake.insert(1, snake[-1])
             snake.pop(-1)
-            win.fill((0, 0, 0))
-            draw_window(win, snake, food, score)
-            pygame.display.update()
 
-            if snake[0].xcoord == food.xcoord and snake[0].ycoord == food.ycoord:
-                snake.append(Body(-1, -1))
+            if np.all(snake[0].coords == food.coords):
+                snake.append(Body(np.array([-1, -1])))
                 score += 1
                 ge[i].fitness += 75
                 food = Food()  # generates new food
@@ -230,12 +220,12 @@ def main(genomes, config):
 
             # checks if the snake has hit itself
             for body in snake[1:]:
-                if snake[0].xcoord == body.xcoord and snake[0].ycoord == body.ycoord:
+                if np.all(snake[0].coords == body.coords):
                     alive = False
                     ge[i].fitness -= 15
 
             # checks if the snake has hit a wall
-            if snake[0].xcoord < 0 or snake[0].ycoord < 0 or snake[0].xcoord >= GRID_X or snake[0].ycoord >= GRID_Y:
+            if snake[0].coords[0] < 0 or snake[0].coords[1] < 0 or snake[0].coords[0] >= GRID_X or snake[0].coords[1] >= GRID_Y:
                 alive = False
                 ge[i].fitness -= 15
 
@@ -254,6 +244,8 @@ def main(genomes, config):
                 ge.pop(i)
 
 
+
+
 def run(config_path):
     config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction,
                                 neat.DefaultSpeciesSet, neat.DefaultStagnation,
@@ -265,7 +257,9 @@ def run(config_path):
     p.add_reporter(stats)
 
     winner = p.run(main, 50)
-
+    with open("winner.pkl", "wb") as f:
+        pickle.dump(winner, f)
+        f.close()
 
 if __name__ == "__main__":
     local_dir = os.path.dirname(__file__)
